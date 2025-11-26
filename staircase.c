5810a34d89006cad6466ff7bc60a4a755841d13d
Gauri Kale <kgauri8989@gmail.com>
2025-11-26 17:24:21 +0530

    Create staircase.c
    Program to print right alligned staircase of size n with its base and height both equal to n.

---
+++ staircase.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter n: ");
+	fflush(stdout);
+	scanf("%d",&n);
+
+	if(n>100)
+		printf("Not valid");
+	else if(n<=0)
+		printf("Not valid");
+	else
+	{
+		printf("Staircase function is given below\n");
+	    int i,j;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n-i;j++)
+			{
+				printf(" ");
+			}
+			for(j=1;j<=i;j++)
+			{
+				printf("#");
+			}
+			printf("\n");
+		}
+	}
+
+return 0;
+}
+}
